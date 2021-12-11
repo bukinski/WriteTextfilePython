4896734b9b1f51cf3dd93e54fc07445d763738f7
gregor szczypek <gregor.szczypek@gmail.com>
2021-12-11 13:34:27 +0100

    Starting point version 1

---
+++ save_logins_in_list.py
@@ -0,0 +1,29 @@
+import time
+
+print("Welcome to 'save logins' program!")
+
+next_dataset = input("Would like to write new data? yes/no: ")
+
+while next_dataset=="yes":
+    f = open("logindata.txt", "a")
+
+    service = input("Enter service: ")
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    
+
+    f.write(service+"\n")
+    f.write(username+"\n")
+    f.write(password+"\n")
+    f.write("\n")
+
+    next_dataset = input("Would you like to save another login dataset? yes/no: ")
+else:
+    print("Entered logins are saved!")
+    time.sleep(1)
+    readout_logins = input("Would you like to read out your current logins? yes/no: ")
+    if readout_logins == "yes":
+        f = open("logindata.txt", "r")
+        content = f.read()
+        print(content)
+    
